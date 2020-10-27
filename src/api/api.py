4cda923289beab7d3ade2019003d2b3ddea9c675
Daniel Hansen <danielhansen@Daniels-MacBook-Air.local>
2020-10-27 10:19:05 +0100

    Created context manager first draft

--- src/api/api.py
+++ src/api/api.py
@@ -7,19 +7,18 @@ import database
 # A reference for relative path for this file to ensure that we're at the correct directory
 dirname = os.path.dirname(__file__)
 
+with database.DBHandler() as db:
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    # Routes for the REST API
+    @app.route('/')
+    def hello():
+        return f'Hello, world!'
 
-# Routes for the REST API
+    # GET
 
-@app.route('/')
-def hello():
-    return f'Hello, world!'
+    # POST
 
-# GET
+    # PUT
 
-# POST
-
-# PUT
-
-# DELETE
+    # DELETE
--- src/api/database.py
+++ src/api/database.py
@@ -1,4 +1,5 @@
 import os
+import firebase_admin
 from firebase_admin import credentials, firestore, initialize_app
 
 dirname = os.path.dirname(__file__)
@@ -8,8 +9,17 @@ cred = credentials.Certificate(dbAuthKey)
 firebase_admin.initialize_app(cred)
 
 class DBHandler:
-    def __init__():
+    def __enter__(self):
+        pass
+
+    def __init__(self):
+        #Create DB if it does not exist (This is part of FTS for new modules)
         db = firestore.client()
         food_ref = db.collection('foods')
         drinks_ref = db.collection('drinks')
         snacks_ref = db.collection('snacks')
+
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
