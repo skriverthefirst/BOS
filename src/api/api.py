43002b22ecae2c69e1d01c93976de01e79e19f09
Daniel Hansen <danielhansen@Daniels-MacBook-Air.local>
2020-10-27 14:52:27 +0100

    branch switch

--- src/api/api.py
+++ src/api/api.py
@@ -7,27 +7,42 @@ import database
 # A reference for relative path for this file to ensure that we're at the correct directory
 dirname = os.path.dirname(__file__)
 
+app = Flask(__name__)
+
 with database.DBHandler() as db:
-    app = Flask(__name__)
+    # Revert debug when development is done
 
     @app.route('/')
     def hello():
-        return f'Hello, world!'
+        return {'products' : ['Food', 'Drinks', 'Snacks']}
 
-    # PUT
-    @app.route('/putFood', methods=['PUT'])
-    def put_food():
+    @app.route('/putOrder', methods=['PUT'])
+    def put_order():
+        '''
+            Orders should come in as followes:
+            {
+                Food : [
+                    "Food nr. 1" : "Amount",
+                    "Food nr. 2" : "Amount",
+                    "Food nr. 3" : "Amount",
+                    ],
+                Drinks : [
+                    "Drink nr. 1" : ["Amount", "Size"],
+                    "Drink nr. 2" : ["Amount", "Size"],
+                    "Drink nr. 3" : ["Amount", "Size"],
+                    ],
+                Snacks : [
+                    "Snack nr. 1" : ["Amount", "Size"],
+                    "Snack nr. 2" : ["Amount", "Size"],
+                    "Snack nr. 3" : ["Amount", "Size"],
+                ]
+            }
+        '''
+
         # Send request to drinks / kitchen
         db.put_food()
-
-    @app.route('/putDrinks', methods=['PUT'])
-    def put_drinks():
-        # Send request to drinks / kitchen
         db.put_drinks()
-
-    @app.route('/putSnacks', methods=['PUT'])
-    def put_snacks():
-        # Send request to drinks / kitchen
         db.put_snacks()
 
-    # DELETE
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
